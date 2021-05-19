5e093405bbe2e011e81066ed040fca90c0f3abfb
Marco Aring <contato@marcoaring.com.br>
2021-05-19 18:35:14 -0300

    chore: change index and insert new dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv-safe';
 import express from 'express';
 
 import settings from './controllers/settings';
+import auth from './controllers/auth';
 
-dotenv.config({ path: './.env' });
+dotenv.config();
+
 axios.defaults.headers.common.Authorization = `Bearer ${process.env.AUTHORIZATION_MOVIEDB}`;
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
-app.use('/api', [settings]);
+app.use(express.json());
+app.use('/api', [auth, settings]);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
