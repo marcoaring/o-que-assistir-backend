c176bbc6822fdcd6a46fa1c1a064e5799c4a8c71
Marco Aring <contato@marcoaring.com.br>
2021-06-02 18:01:23 -0300

    feat: add router to get all streamings

--- src/controllers/movies/index.js
+++ src/controllers/movies/index.js
@@ -27,4 +27,34 @@ movies.get('/films/genres', authMiddleware, async (req, res) => {
   }
 });
 
+movies.get('/films/streamings', authMiddleware, async (req, res) => {
+  try {
+    const url = `${process.env.API_URL_MOVIEDB}watch/providers/movie`;
+    defaultParams.watch_region = 'BR';
+    const streamings = [];
+    const { results } = (await axios.get(url, { params: defaultParams })).data;
+
+    results.map((streaming) => {
+      streamings.push({
+        id: streaming.provider_id,
+        logo: `${process.env.NODE_ENV === 'dev' ? 'http' : 'https'}://image.tmdb.org/t/p/original${
+          streaming.logo_path
+        }`,
+        name: streaming.provider_name,
+      });
+    });
+
+    streamings.sort((a, b) => {
+      const x = a.name.toUpperCase();
+      const y = b.name.toUpperCase();
+
+      return x === y ? 0 : x > y ? 1 : -1;
+    });
+
+    return res.json({ streamings, status: 'Sucesso' });
+  } catch (err) {
+    return res.status(400).send({ error: 'Falha ao receber os stramings.' });
+  }
+});
+
 export default movies;
