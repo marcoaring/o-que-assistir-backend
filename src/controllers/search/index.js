7cd34ec9aec1fb774ce0410c0ff27a5455ee0c3e
Marco Aring <contato@marcoaring.com.br>
2021-06-03 13:56:06 -0300

    feat: insert search movie router

--- src/controllers/search/index.js
+++ src/controllers/search/index.js
@@ -48,4 +48,86 @@ search.get('/search/actor', authMiddleware, async (req, res) => {
   }
 });
 
+search.get('/search/movie', authMiddleware, async (req, res) => {
+  try {
+    const params = {
+      sort_by: 'popularity.desc',
+    };
+
+    if (req.query.actors) {
+      params.with_people = req.query.actors;
+    }
+
+    if (req.query.genres) {
+      params.with_genres = req.query.genres;
+    }
+
+    if (req.query.year) {
+      params.primary_release_year = req.query.year;
+    }
+
+    if (req.query.streamings) {
+      params.with_watch_providers = req.query.streamings;
+    }
+
+    Object.assign(params, defaultParams);
+
+    axios.get(`${process.env.API_URL_MOVIEDB}discover/movie`, { params }).then((response) => {
+      const promises = response.data.results.map((movie) =>
+        axios
+          .get(`${process.env.API_URL_MOVIEDB}movie/${movie.id}/watch/providers`)
+          .then((streamings) => {
+            const streaming = {
+              buy: [],
+              rent: [],
+            };
+
+            if (streamings.data.results.BR) {
+              streamings.data.results.BR.buy.map((streamingBuy) => {
+                streaming.buy.push({
+                  id: streamingBuy.provider_id,
+                  logo: `${
+                    process.env.NODE_ENV === 'dev' ? 'http' : 'https'
+                  }://image.tmdb.org/t/p/original${streamingBuy.logo_path}`,
+                  name: streamingBuy.provider_name,
+                });
+              });
+
+              streamings.data.results.BR.rent.map((streamingRent) => {
+                streaming.rent.push({
+                  id: streamingRent.provider_id,
+                  logo: `${
+                    process.env.NODE_ENV === 'dev' ? 'http' : 'https'
+                  }://image.tmdb.org/t/p/original${streamingRent.logo_path}`,
+                  name: streamingRent.provider_name,
+                });
+              });
+            }
+
+            return {
+              background: `${
+                process.env.NODE_ENV === 'dev' ? 'http' : 'https'
+              }://image.tmdb.org/t/p/original${movie.backdrop_path}`,
+              description: movie.overview,
+              genres: movie.genre_ids,
+              id: movie.id,
+              popularity: movie.popularity,
+              poster: `${
+                process.env.NODE_ENV === 'dev' ? 'http' : 'https'
+              }://image.tmdb.org/t/p/original${movie.poster_path}`,
+              release_date: movie.release_date,
+              streamings: streamings.data.results.BR ? streaming : null,
+              title: movie.title,
+              vote: movie.vote_average,
+            };
+          })
+      );
+
+      return Promise.all(promises).then((movies) => res.json({ movies, status: 'Sucesso' }));
+    });
+  } catch (err) {
+    return res.status(400).send({ error: 'Falha ao encontrar o filme.' });
+  }
+});
+
 export default search;
