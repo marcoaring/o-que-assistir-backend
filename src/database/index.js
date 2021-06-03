6ec33a759fd8e9898b94c4138f02af02d223d040
Marco Aring <contato@marcoaring.com.br>
2021-06-03 13:57:50 -0300

    fix: avoid console warn of mongo

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 mongoose.Promise = global.Promise;
 
